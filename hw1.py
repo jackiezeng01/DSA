d536a786f2ff76904cd9d913299769d3b64cde48
Jackie Zeng <dolphin.pool01@gmail.com>
2021-02-02 19:35:46 -0500

    add homework files

---
+++ hw1.py
@@ -0,0 +1,13 @@
+"""
+[Insert description]
+"""
+
+__author__ = "[Insert your name here]"
+
+
+def find_max_val_unimodal_arr(unimodal_arr):
+    """
+    [Fill out docstring]
+    """
+    # Complete function
+    pass
---
+++ hw1_student_tests.py
@@ -0,0 +1,25 @@
+"""
+[Insert description]
+"""
+
+import pytest
+
+__author__ = "[Insert your name here]"
+
+# You are welcome to use a different format for your pytest testing; this is
+# just a suggested way to do it. Something that you *must* keep with your
+# testing, however, is the timeout decorator: "@pytest.mark.timeout(1)".
+
+test_cases = [
+    # Insert test cases here.
+]
+
+
+@pytest.mark.timeout(1)
+@pytest.mark.parametrize("test_case", test_cases)
+def test_find_max_val_unimodal_arr(test_case):
+    """
+    [Fill out docstring]
+    """
+    # Complete function
+    pass
